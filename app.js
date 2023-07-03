0514a63ea686c3d88d66aecb7026e8b23c125524
RafaelNavon <raffi158@gmail.com>
2023-07-03 23:19:23 +0300

    added to mongoDB

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27017/todolistDB", {useNewUrlParser: true});
+mongoose.connect("mongodb+srv://admin-Rafael:Rafael1992@cluster0.e0irf4s.mongodb.net/todolistDB", {useNewUrlParser: true});
 
 const itemsSchema = {
     name: String,
